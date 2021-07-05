36af29dfb1cd8d10e21abc22520da388e36bb175
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-07-05 15:14:53 +0200

    change code layout

--- landing/src/main.js
+++ landing/src/main.js
@@ -7,11 +7,12 @@ Vue.use(CarbonComponentsVue);
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 new Vue({
   router,
   apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app');
-Vue.use(BootstrapVue);
-Vue.use(BootstrapVueIcons);
+
